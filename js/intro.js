8037cd29bc7b769875bf17d184a36dc272d8d07f
Ryan <126Rliu@frhsd.com>
2025-10-27 18:06:19 -0400

    first commit

---
+++ js/intro.js
@@ -0,0 +1,16 @@
+//   document.getElementById('intro').style.animation = 'none';
+//   document.getElementById('start').style.animation = 'none';
+//   document.getElementById('main').style.animation= 'none';
+
+// document.getElementById("start").addEventListener("click", function(){
+//     document.getElementById('start').style.animation = '';
+
+//     setInterval(function(){
+//         document.getElementById('intro').style.animation = '';
+//         setInterval(function(){
+//             document.getElementById('intro').style.display = 'none';
+//             document.getElementById('main').style.display = 'block';
+//             document.getElementById('main').style.animation = '';
+//         }, 1000);
+//     }, 700);
+// })
